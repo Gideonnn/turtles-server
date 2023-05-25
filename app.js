365bdf8c0acb4981f09eddce690415314ae5c493
Gideon Spierings <gideon@nexusmutual.io>
2023-05-25 23:28:40 +0200

    Add custom metrics

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const logger = require("morgan");
 const cors = require("cors");
 const data = require("./data.json");
 
+const tx2 = require("tx2");
+
+const meter = tx2.meter({
+  name: "Requests per second",
+  samples: 1,
+  timeframe: 60,
+});
+
 const app = express();
 
 app.use(logger("dev"));
@@ -10,7 +18,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+let totalRequestCounter = 0;
+
+const totalReqs = tx2.metric({
+  name: "Total Requests",
+  value: () => totalRequestCounter,
+});
+
 app.get("/", (_, res) => {
+  meter.mark();
+  totalRequestCounter++;
+  totalReqs.set(totalRequestCounter);
   res.json(data);
 });
 
